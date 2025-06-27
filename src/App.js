60371e60b786dc4480606df933ff138cfa5eb6c7
nilesh-patil23 <168561348+nilesh-patil23@users.noreply.github.com>
2025-06-27 15:26:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'; // Importing the CSS file for styles
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text">Welcome to the Accleration Training Center...</h1>
+    </div>
+  );
+};
+
+export default App;
